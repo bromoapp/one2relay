97aa2dd338edb20cd0c98edb171ae1f541c5df6a
B Kunto Adji <bromoapptester@gmail.com>
2017-08-16 07:36:32 +0700

    Update dev.exs

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :one2relay, One2relay.Repo,
   adapter: Ecto.Adapters.MySQL,
-  username: "root",
-  password: "admin",
+  username: "myapp",
+  password: "something",
   database: "one2relay_dev",
   hostname: "localhost",
   pool_size: 10
